6699f31fe165fd0170b8540238578ca9fd3e6217
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-03 08:26:35 -0400

    Refactor code using a function definition that shifts characters

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,5 +1,8 @@
 #Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string
-
+def shift_char(char,chars,shift_factor)
+    i=chars.find_index(char)
+    chars[(i+shift_factor)%26]
+end
 
 def ceaser_cipher(string,shift_factor)
     down_ascii_chars=("a".."z").map{|letter| letter}
@@ -7,11 +10,9 @@ def ceaser_cipher(string,shift_factor)
     chars= string.split("")
     shifted_chars= chars.map { |char| 
         if up_ascii_chars.include?(char)
-            i=up_ascii_chars.find_index(char.upcase)
-            up_ascii_chars[(i+shift_factor)%26]
+            shift_char(char,up_ascii_chars,shift_factor)
         elsif down_ascii_chars.include?(char)
-            i=down_ascii_chars.find_index(char.downcase)
-            down_ascii_chars[(i+shift_factor)%26]
+            shift_char(char,down_ascii_chars,shift_factor)
         else
             char
         end
